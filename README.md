b2465ee68d929ccf10f4a5dab8070b3de9352272
Luugui <luisguilhermester@live.com>
2021-10-14 09:07:54 -0300

    Update README; Added requirements

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # ZABBIX EXPORT TREND
 
-PT-BR | EN
+![](https://img.shields.io/badge/Zabbix-5.0-green) ![](https://img.shields.io/badge/Python-3.9-blue)
+
+---
+### Descrição
+
+Script criado para extração de trends historicos de um host selecionado. Neste relatório será gerado um csv com os itens de CPU, Memória, Disco e In/Out das Interfaces de Rede.
+
+---
+
+###  Dependências
+
+- pyzabbix
+- tqdm
+- argparse
+
+`pip install pyzabbix tqdm argparse`
+
+---
+### Exemplo
 
 
+`python zabbix-export-trends.py -u Admin -p zabbix -s http://localhost/zabbix -n 'Zabbix Server' -f 01/01/2021 -t 02/01/2021`
 
+- "-u" - Login Zabbix
+- "-p" - Senha do Login
+- "-s" - URL do Console Zabbix
+- "-n" - Nome do Host
+- "-f" - Data inicio do relatório
+- "-t" - Data final do relatório
--- zabbix-export-trend.py
+++ zabbix-export-trend.py
@@ -6,7 +6,7 @@
 #email               : luisguilhermester at live dot com
 #date                : 2021/10/13
 #version             : 0.1.0
-#usage               : $ python zabbix-export-trends.py -u Admin -p zabbix -s http://localhost/zabbix -h 'Zabbix Server' -f 01/01/2021 -f 02/01/2021
+#usage               : $ python zabbix-export-trends.py -u Admin -p zabbix -s http://localhost/zabbix -n 'Zabbix Server' -f 01/01/2021 -t 02/01/2021
 #notes               : All parameters are required
 #license             : Apache-2.0
 #python_version      : 3.9.2
@@ -15,9 +15,9 @@
 from pyzabbix import ZabbixAPI
 from tqdm import tqdm
 from datetime import datetime
-import time, csv, sys, argparse
+import time, csv, os, argparse
 
-# INITIALIZE ARGUMENTS
+# Configurando os argumentos // Configuring the arguments
 
 parser = argparse.ArgumentParser(description="Extract trend from select host")
 parser.add_argument("-u", "--user", required=True, help="Zabbix user")
@@ -28,18 +28,19 @@ parser.add_argument("-f", "--from", required=True, help="Time from")
 parser.add_argument("-t", "--till", required=True, help="Time till")
 args = vars(parser.parse_args())
 
-# VARIAVEIS DE CONFIGURAÇÃO DO RELATÓRIO
+# VARIAVEIS DE CONFIGURAÇÃO DO RELATÓRIO //  REPORT'S VARIABLES 
 URL=args['server']
 LOGIN=args['user']
 SENHA=args['password']
 SERVIDOR=args['name']
 INICIO=f"{args['from']}"
 FIM=f"{args['till']} 23:59:59"
+REPORT_NAME = f'ZABBIX-{SERVIDOR}-TREND-EXPORT.csv'
 
 
 
 
-# CONECTANDO AO ZABBIX
+# CONECTANDO AO ZABBIX // CONNECT TO ZABBIX
 app = ZabbixAPI(URL)
 if "https" in URL:
     import requests
@@ -50,7 +51,7 @@ print(f"[{datetime.now()}] Connected to Zabbix!\n")
 
 LIST_HOSTS = app.host.get(output=["hostid","name"],filter={"host": SERVIDOR},limit=1)
 
-# Função para coleta dos ids dos items de CPU, Memória e Disco
+# Função para coleta dos ids dos items de CPU, Memória, Disco e Rede // Function to collect ids from items (CPU, Mem, Disk and Interfaces)
 
 def get_metrics(host):
     try:
@@ -72,7 +73,7 @@ def get_metrics(host):
     except:
         return 0
 
-# Função para converter data em Timestamp aceito pelo Zabbix
+# Função para converter data em Timestamp aceito pelo Zabbix // Function to convert human date to timestam accepted by Zabbix
 
 def date_to_timestamp(data):
     try:
@@ -86,7 +87,7 @@ def date_to_timestamp(data):
 
         return DATA_INICIO
 
-# Função para converter Timestamp do Zabbix para data legivel
+# Função para converter Timestamp do Zabbix para data legivel // Function to convert timestamp to human date
 
 def timestamp_to_date(tm):
     return time.strftime("%d/%m/%Y %H:%M:%S", time.localtime(int(tm)))
@@ -94,9 +95,10 @@ def timestamp_to_date(tm):
 
 
 def main():
-    with open(f'ZABBIX-{SERVIDOR}-TREND-EXPORT_{sys.argv[4]}.csv', mode='w', newline='') as csv_file:
+    os.system('cls') if os.name == 'nt' else os.system('clear')
+    with open(REPORT_NAME, mode='w', newline='') as csv_file:
         trend_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        trend_writer.writerow(['HOST','DATA','METRICA','VALUE AVG'])
+        trend_writer.writerow(['HOST','DATE','METRIC','VALUE AVG'])
         for host in LIST_HOSTS:
             trend = app.trend.get(output=["itemid","clock","value_avg"], itemids=get_metrics(host['hostid']), time_from=date_to_timestamp(INICIO), time_till=date_to_timestamp(FIM))
             for t in tqdm(trend, ascii=True, desc=f"[{datetime.now()}] Extract trends from {SERVIDOR}"):
